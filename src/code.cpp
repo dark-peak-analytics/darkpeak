4ffa43c55b58ea660655dbae32d80a77db109a0a
Robert Smith <rasmith3@sheffield.ac.uk>
2020-10-29 12:45:44 +0000

    adding armamarkov

--- src/code.cpp
+++ src/code.cpp
@@ -1,4 +1,5 @@
 #include <Rcpp.h>
+//#include <RcppArmadillo.h>
 using namespace Rcpp;
 
 
@@ -6,4 +7,17 @@ using namespace Rcpp;
 int timesFour(int x) {
   return x * 4;
 }
+
+// [[Rcpp::depends(RcppArmadillo)]]
+//arma::mat estimate_markov_armadillo_and(arma::mat m_TR, arma::cube& a_P )
+//{
+//  int rows = m_TR.n_rows;
+//
+//  for(int i = 1; i < rows; i++){
+//    m_TR.row(i) = m_TR.row(i-1) * a_P.slice(i);
+//  }
+//
+//  return m_TR;
+//}
+
 
