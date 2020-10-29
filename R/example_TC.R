e0a7564aeb87a9c7dc81f3b9c7b1c8b40a56375c
Robert Smith <rasmith3@sheffield.ac.uk>
2020-10-29 17:27:34 +0000

    including example data

---
+++ R/example_TC.R
@@ -0,0 +1,17 @@
+#' Example total costs matrix
+#'
+#' @docType data
+#'
+#' @usage data(total_costs)
+#'
+#' @format An object of class \code{"matrix"}.
+#'
+#' @keywords datasets
+#'
+#' @references Smith & Schneider (2020)
+#' (\href{https://wellcomeopenresearch.org/articles/5-69}{WellcomeOpen})
+#'
+#' @source None
+#'
+#' @examples
+#' data(total_costs)
---
+++ R/example_TQ.R
@@ -0,0 +1,17 @@
+#' Example total QALYs matrix
+#'
+#' @docType data
+#'
+#' @usage data(total_qalys)
+#'
+#' @format An object of class \code{"matrix"}.
+#'
+#' @keywords datasets
+#'
+#' @references Smith & Schneider (2020)
+#' (\href{https://wellcomeopenresearch.org/articles/5-69}{WellcomeOpen})
+#'
+#' @source None
+#'
+#' @examples
+#' data(total_qalys)
