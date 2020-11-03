2d5a934fd174a1a1f0862ec732c0fe2ef967dacb
Robert Smith <rasmith3@sheffield.ac.uk>
2020-11-03 19:00:56 +0000

    removed unnecessary cpp comments

--- src/code.cpp
+++ src/code.cpp
@@ -8,16 +8,6 @@ int timesFour(int x) {
   return x * 4;
 }
 
-// [[Rcpp::depends(RcppArmadillo)]]
-//arma::mat estimate_markov_armadillo_and(arma::mat m_TR, arma::cube& a_P )
-//{
-//  int rows = m_TR.n_rows;
-//
-//  for(int i = 1; i < rows; i++){
-//    m_TR.row(i) = m_TR.row(i-1) * a_P.slice(i);
-//  }
-//
-//  return m_TR;
-//}
+
 
 
